094bd6269c2a78de03fe9063c6e40864085e638e
santomon <simon.bohlender@kgu.de>
2025-12-14 17:27:02 +0100

    first draft by using string contents (we are weak to localization :(. need to adapt this by using good heuristics for the dom paths

--- src/google-less-ai.ts
+++ src/google-less-ai.ts
@@ -1,4 +1,5 @@
 const AI_MODE_TAB_CLASSNAME = "data-ai-mode-tab"
+const AI_OVERVIEW_CLASSNAME = "data-ai-overview-div"
 
 
 const hideAndMarkAITabNode = (spanElement: HTMLElement) => {
@@ -15,7 +16,7 @@ const hideAndMarkAITabNode = (spanElement: HTMLElement) => {
 
     if (nthParent.getAttribute('role') === 'listitem' &&
         nPlus1thParent?.getAttribute('role') === 'list') {
-        nthParent.hidden = true;
+        nthParent.style.display = 'none'
         nthParent.classList.add(AI_MODE_TAB_CLASSNAME)
 
         return true
@@ -24,18 +25,52 @@ const hideAndMarkAITabNode = (spanElement: HTMLElement) => {
 
 }
 
-const isSearchedNode = (node: Node): node is HTMLElement => {
-    return node instanceof HTMLElement && node.tagName === "SPAN" && node.textContent?.trim().includes("AI Mode")
+const hideAndMarkAIOverviewNode = (spanElement: HTMLElement) => {
+
+    let nthParent: HTMLElement | null = spanElement
+
+    for (let i = 0; i < 14; i++) {
+        nthParent = nthParent.parentElement
+        if (!nthParent) return false;
+    }
+
+    const nPlus1thParent = nthParent.parentElement
+
+
+    if (true) {
+        nthParent.style.display = 'none'
+        nthParent.classList.add(AI_OVERVIEW_CLASSNAME)
+
+
+        console.log("making hidden:", nthParent)
+        return true
+    }
+    return false
+
+}
+const isAITabNode = (node: Node): node is HTMLElement => {
+    return node instanceof HTMLElement && node.tagName === "SPAN" && node.innerText?.trim() ==="AI Mode"
+}
+
+
+const isAIOverviewNode = (node: Node): node is HTMLElement => {
+    return node instanceof HTMLElement && node.tagName == "SPAN" && node.innerText?.trim() === "An AI Overview is not available for this search"
 }
 
 const observer = new MutationObserver(mutations => {
+    let didHideAITabNode = false
+    let didHideAIOverviewNode = false
+
+
     for (const mutation of mutations) {
         for (const addedNode of Array.from(mutation.addedNodes)) {
-            if (isSearchedNode(addedNode)) {
-                const didHideNode = hideAndMarkAITabNode(addedNode)
-                if (didHideNode) {
-                    return
-                };
+            if (!didHideAITabNode && isAITabNode(addedNode)) {
+                didHideAITabNode = hideAndMarkAITabNode(addedNode)
+            }
+
+            if (!didHideAIOverviewNode && isAIOverviewNode(addedNode)) {
+                console.log("found the cuplrit", addedNode, addedNode.textContent)
+                didHideAIOverviewNode = hideAndMarkAIOverviewNode(addedNode)
             }
         }
     }
